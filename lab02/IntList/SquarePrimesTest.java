48d22378d845fede63f7f866316195e0237144e3
effycoco <734157786@qq.com>
2022-11-22 19:02:33 +0800

    finish lab02 - debugging and JUnit test

--- lab02/IntList/SquarePrimesTest.java
+++ lab02/IntList/SquarePrimesTest.java
@@ -17,4 +17,20 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesAtEnds(){
+        IntList lst = IntList.of(3,6,13,4,17);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("9 -> 6 -> 169 -> 4 -> 289", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesNoPrime(){
+        IntList lst = IntList.of(4,18,32,20,12);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 18 -> 32 -> 20 -> 12",lst.toString());
+        assertFalse(changed);
+    }
 }
